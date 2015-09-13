7f36eefeeedfaf431611c368b668b8d939c9d16d
John Stinson <johnstinson99@gmail.com>
2015-09-13 16:08:46 +0100

    testing infinitest

--- src/main/java/imager/domain/Transforms/FastPrintable4by4Matrix.java
+++ src/main/java/imager/domain/Transforms/FastPrintable4by4Matrix.java
@@ -46,7 +46,7 @@ public class FastPrintable4by4Matrix /*extends MatrixPrintable*/ {
         float z = myColoredPVector.getZ();
         //System.out.println(A[0][0] + "*" + x + " + " + A[0][1] + "*" + y + " + " + A[0][2] + "*" + z + " + "+A[0][3]);
         //System.out.println(A[1][0] + "*" + x + " + " + A[1][1] + "*" + y + " + " + A[1][2] + "*" + z + " + "+A[1][3]);
-        myColoredPVector.setX((float)(A[0][0]*x + A[0][1]*y + A[0][2]*z + A[0][3]));
+         myColoredPVector.setX((float)(A[0][0]*x + A[0][1]*y + A[0][2]*z + A[0][3]));
         myColoredPVector.setY((float)(A[1][0]*x + A[1][1]*y + A[1][2]*z + A[1][3]));
         myColoredPVector.setZ((float) (A[2][0] * x + A[2][1] * y + A[2][2] * z + A[2][3]));
     }
--- src/test/java/cucumber_features_and_stepdefs/CucumberRunner.java
+++ src/test/java/cucumber_features_and_stepdefs/CucumberRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 /**
  * John Stinson 26/07/2015
- * Roman Numbers Kata - Runner for Cucumber
+ * Runner for Cucumber
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "json:target/cucumber-report.json")
